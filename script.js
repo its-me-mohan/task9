120403a6046d3b050fd8f58c0f4b80adb1aebb39
Mohan Raj <156092217+its-me-mohan@users.noreply.github.com>
2024-07-18 17:09:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+let timer=document.createElement("div");
+timer.setAttribute("id","countdown");
+
+document.body.append(timer);
+
+setTimeout(()=>{
+    timer.innerHTML="10";
+    setTimeout(()=>{
+        timer.innerHTML="9";
+        setTimeout(()=>{
+            timer.innerHTML="8";
+            setTimeout(()=>{
+                timer.innerHTML="7";
+                setTimeout(()=>{
+                    timer.innerHTML="6";
+                    setTimeout(()=>{
+                        timer.innerHTML="5";
+                        setTimeout(()=>{
+                            timer.innerHTML="4";
+                            setTimeout(()=>{
+                                timer.innerHTML="3";
+                                setTimeout(()=>{
+                                    timer.innerHTML="2";
+                                    setTimeout(()=>{
+                                        timer.innerHTML="1";
+                                        setTimeout(()=>{
+                                            timer.innerHTML="HAPPY INDEPENDENCE DAY";
+                                        },1000);
+                                    },1000);
+                                },1000);
+                            },1000);
+                        },1000);
+                    },1000);
+                },1000);
+            },1000);
+        },1000);
+    },1000);
+},1000);
+
+   
+
+
